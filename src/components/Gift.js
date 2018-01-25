c9302ead8885d9de5bb22e85a319a5c5f3ebcfe1
Victor Perez <vicperez@cobogpgp3428.global.publicisgroupe.net>
2018-01-25 15:26:33 -0500

    feature: adds a Gift component

---
+++ src/components/Gift.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { Form, FormGroup, FormControll, ControlLabel, Button, FormControl } from 'react-bootstrap'
+
+class Gift extends Component {
+    constructor() {
+        super()
+
+        this.state = { person: '', present: '' }
+    }
+    render() {
+        return (
+            <div>
+                <Form>
+                    <FormGroup>
+                        <ControlLabel>Person</ControlLabel>
+                        <FormControl
+                            className='input-person'
+                            onChange={event => this.setState({ person: event.target.value })}
+                        />
+                    </FormGroup>
+                </Form>
+            </div>
+        )
+    }
+}
+
+export default Gift
